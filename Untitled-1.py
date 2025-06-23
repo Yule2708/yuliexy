c66f92f6a1ce586b9086480530b6c741ae30250d
Yule2708 <yuliexydimas@gmail.com>
2025-06-23 17:08:49 -0400

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,21 @@
+
+#ejercicio 1
+num1= input("Dame un numero: ")
+print(f"Tu primer numero es:  {num1}")
+num2= int(input ("Dame otro numero: "))
+print (f"Tu segundo numero es: {num2}")
+
+suma= num1 + num2
+resta= num1- num2
+Multiplicacion= num1 * num2
+divicion= num1 / num2
+
+resultado1= suma
+resultado2=resta
+resultado3=Multiplicacion
+resultado4= divicion
+
+print(resultado1)
+print(resultado2)
+print(resultado3)
+print(resultado4)
