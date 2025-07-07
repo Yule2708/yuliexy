e805389d2dd31e447fdda198761b833c582a31eb
Yule2708 <yuliexydimas@gmail.com>
2025-07-07 14:35:32 -0400

    calculadora

---
+++ Modulo-3/calculadora.php
@@ -0,0 +1,12 @@
+<?
+
+$numero1 = 45;
+$numero2 = 20;
+
+echo "Suma: " . ($numero1 + $numero2) . "\n";          
+echo "Resta: " . ($numero1 - $numero2) . "\n";         
+echo "Multiplicación: " . ($numero1 * $numero2) . "\n"; 
+echo "División: " . ($numero1 / $numero2) . "\n";       
+echo "Módulo: " . ($numero1 % $numero2) . "\n\n";    
+
+?>
