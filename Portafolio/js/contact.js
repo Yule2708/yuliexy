b93d9c4687372e4432d73b31840d6c56fe967dd1
Yule2708 <yuliexydimas@gamil.com>
2025-07-10 14:42:48 -0400

    cambio 2156465321561

--- Portafolio/js/contact.js
+++ Portafolio/js/contact.js
@@ -1,7 +1,7 @@
 const contactForm = document.getElementById('contact-form'),
   contactName = document.getElementById('contact-name'),
   contactEmail = document.getElementById('contact-email'),
-  contactSudbject = document.getElementById('contact-subject'),
+  contactSubject = document.getElementById('contact-subject'),
   contactMessage = document.getElementById('contact-message'),
   message = document.getElementById('message');
 
@@ -11,11 +11,39 @@ const contactForm = document.getElementById('contact-form'),
     if(contactName.value == '' || contactEmail.value === "" || contactSudbject.value === '' || 
         contactMessage.value === '' ){
             message.textContent = 'Write all the input fields';
-            message.classList.add ('color-red')
+            message.classList.remove('color-first');
+            message.classList.add ('color-red');
 
             setTimeout(() =>{
                 message.textContent =''
             }, 3000);
+        } else{
+            emailjs.
+            sendForm(
+                'service_640j641', 
+                'template_4vf9c4t', 
+                '#contact-form', 
+                'jByhQgDtLJHN0KgWC'
+            )
+                .then(
+                () => {
+                    message.textContent = 'Message sent ';
+                    message.classList.add ('color-first');
+
+                    setTimeout(() =>{
+                        message.textContent =''
+                    }, 5000);
+                },
+                (error) => {
+                  alert('OOPs! SOMETHING WENT WRONG...', error);
+
+                },
+              );
+
+              contactName.value ='';
+              contactEmail.value ='';
+              contactSubject.value ='';
+              contactMessage.value ='';
         }
   };
 
